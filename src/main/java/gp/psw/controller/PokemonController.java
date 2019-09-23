59cf5fce2336f865eae235836487486f05b37162
tristan57360 <Tristan@DESKTOP-8TUEOPG>
2019-09-23 18:38:46 +0200

    Modification de la fonction REST PUT avec ajout des codes d'erreurs

--- src/main/java/gp/psw/controller/PokemonController.java
+++ src/main/java/gp/psw/controller/PokemonController.java
@@ -2,13 +2,17 @@ package gp.psw.controller;
 
 import gp.psw.dao.PokemonDAO;
 import gp.psw.entity.Pokemon;
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.MissingResourceException;
 import java.util.Optional;
 
 @RestController
@@ -36,6 +40,7 @@ public class PokemonController {
     }
 
    @PostMapping("/pokemon")
+   @ResponseBody
     public ResponseEntity<Object> createStudent(@RequestBody Pokemon pokemon, @PathVariable final long Id) {
         Pokemon newPokemon = pokemonDAO.save(pokemon);
 
@@ -49,17 +54,13 @@ public class PokemonController {
     }
 
     @PutMapping("/pokemon/{Id}")
-    public ResponseEntity<Object> updateStudent(@RequestBody Pokemon pokemon, @PathVariable final long Id) {
+    @ResponseBody
+    public ResponseEntity<Object> updateStudent(@Valid @RequestBody Pokemon pokemonDetails, @PathVariable final long Id) {
+        Pokemon pokemon = pokemonDAO.findById(Id)
+                .orElseThrow(() -> new ObjectNotFoundException("Pokemon not found for this id :: " + Id,Pokemon.class.getName()));
 
-        Optional<Pokemon> studentOptional = pokemonDAO.findById(Id);
-
-        if (!studentOptional.isPresent())
-            return ResponseEntity.notFound().build();
-
-        pokemon.setId(Id);
-
-        pokemonDAO.save(pokemon);
-
-        return ResponseEntity.noContent().build();
+        pokemon.setName(pokemonDetails.getName());
+        final Pokemon updatedPokemon = pokemonDAO.save(pokemon);
+        return ResponseEntity.ok(updatedPokemon);
     }
 }
