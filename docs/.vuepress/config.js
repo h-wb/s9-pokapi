01d98cc5644d94900a4c3a961fbfd6f94a83173d
root <root@DESKTOP-C1QQ2IL.localdomain>
2020-01-22 18:46:08 +0100

    :construction: Configuration vuepress

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,4 @@ module.exports = {
     base: '/api-psw/',
     title: 'VuePress in Github Pages',
     description: 'Static website built with VuePress and deployed on Github Pages',
-    dest: '/docs'
 };
