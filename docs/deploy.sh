7f547eff1317490dea431627ed8d73adae79806f
h-wb <dev@wehbe.pro>
2020-01-24 19:20:28 +0100

    :construction: test build travis

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env sh
 
+set -e
+
+npm run docs:build
+
 cd docs/.vuepress/dist
 
 git init
 git add -A
 git commit -m 'Deploy'
 
-git push -f git@github.com:h-wb/Pokapi.git develop:gh-pages
+git push -f git@github.com:h-wb/Pokapi.git master:gh-pages
 
 cd -
