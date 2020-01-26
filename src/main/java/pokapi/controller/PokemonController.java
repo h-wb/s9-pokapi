ec466adaaabb5a966ba4674f9c4edfe812102875
Dany Sanchez <daany_57@hotmail.fr>
2020-01-26 23:56:59 +0100

    :sparkles: Ajout de l'export associé à la recherche scorée

--- src/main/java/pokapi/controller/PokemonController.java
+++ src/main/java/pokapi/controller/PokemonController.java
@@ -2,17 +2,32 @@ package pokapi.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pokapi.dto.PokemonDTO;
+import pokapi.entity.EstTypeEntity;
 import pokapi.entity.PokemonEntity;
+import pokapi.entity.TypeEntity;
 import pokapi.exception.ResourceNotFoundException;
-import pokapi.helper.SearchPokemon;
+import pokapi.helper.export.ExportCSV;
+import pokapi.helper.export.ExportXLSX;
+import pokapi.helper.export.version.PokemonsExportVersion;
+import pokapi.helper.export.version.PokemonsExportVersionFull;
+import pokapi.helper.export.version.PokemonsExportVersionLight;
+import pokapi.helper.search.SearchPokemon;
+import pokapi.repository.EstTypeRepository;
 import pokapi.repository.PokemonRepository;
+import pokapi.repository.TypeRepository;
 
 import javax.validation.Valid;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,11 +38,15 @@ import java.util.stream.Collectors;
 @Api(value = "Pokémons", tags = "pokemon")
 public class PokemonController {
     private final PokemonRepository pokemonRepository;
+    private final TypeRepository typeRepository;
+    private final EstTypeRepository estTypeRepository;
     private static final String ERROR_POKEMON_ID_NOT_FOUND = "Pokemon not found for this id :: ";
 
     @Autowired
-    public PokemonController(PokemonRepository pokemonRepository) {
+    public PokemonController(PokemonRepository pokemonRepository, TypeRepository typeRepository, EstTypeRepository estTypeRepository) {
         this.pokemonRepository = pokemonRepository;
+        this.typeRepository = typeRepository;
+        this.estTypeRepository = estTypeRepository;
     }
 
     @ApiOperation(value = "Récuperer tous les pokémons")
@@ -91,7 +110,40 @@ public class PokemonController {
     @ApiOperation(value = "Chercher un Pokémon par nom")
     @GetMapping(value = "/search/", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public List<PokemonEntity> searchPokemon(@RequestParam(value = "Name") String exp) {
-        return SearchPokemon.searchScore((List<PokemonEntity>) pokemonRepository.findAll(), exp);
+    public List<PokemonEntity> searchPokemon(@RequestParam(value = "Name", required = false) String exp) {
+        List<PokemonEntity> pokemonEntities = (List<PokemonEntity>) pokemonRepository.findAll();
+        return StringUtils.isBlank(exp) ? pokemonEntities : SearchPokemon.searchScore(pokemonEntities, exp);
+    }
+
+    @ApiOperation(value = "Exporter des Pokémons par nom")
+    @GetMapping(value = "/export/")
+    @ResponseBody
+    public ResponseEntity<InputStreamResource> exportPokemons(
+            @ApiParam(name = "Extension", value = "Format du fichier", allowableValues = "xlsx, csv", required = true) @RequestParam(value = "Extension") String extension,
+            @ApiParam(name = "Version", value = "Version à exporter", allowableValues = "full, light", required = true) @RequestParam(value = "Version") String version,
+            @RequestParam(value = "Name", required = false) String exp) throws IOException {
+        List<PokemonEntity> pokemonEntities = StringUtils.isBlank(exp) ?
+                (List<PokemonEntity>) pokemonRepository.findAll() :
+                SearchPokemon.searchScore((List<PokemonEntity>) pokemonRepository.findAll(), exp);
+        List<TypeEntity> typeEntities = typeRepository.findAll();
+        List<EstTypeEntity> estTypeEntities = (List<EstTypeEntity>) estTypeRepository.findAll();
+
+        HashMap<String, String> contentTypes = new HashMap<>();
+        contentTypes.put("xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        contentTypes.put("csv", "text/csv; charset=utf-8");
+
+        PokemonsExportVersion pokemonsExportVersion = version.equals("full") ?
+                new PokemonsExportVersionFull(extension, pokemonEntities, typeEntities, estTypeEntities) :
+                new PokemonsExportVersionLight(extension, pokemonEntities);
+
+        ByteArrayInputStream in = extension.equals("xlsx") ? ExportXLSX.export(pokemonsExportVersion) : ExportCSV.export(pokemonsExportVersion);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" +
+                pokemonsExportVersion.getFileName() +
+                (StringUtils.isBlank(exp) ? "" : "_" + exp.trim()) +
+                "." + pokemonsExportVersion.getExtension()
+        );
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentTypes.get(extension))).headers(headers).body(new InputStreamResource(in));
     }
 }
