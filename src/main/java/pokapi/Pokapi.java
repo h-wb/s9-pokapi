e15dd7d87fd2e815255fb5f001dea9569aaccefd
h-wb <dev@wehbe.pro>
2020-01-26 18:48:54 +0100

    :whale: Modification configuration prometheus

--- src/main/java/pokapi/Pokapi.java
+++ src/main/java/pokapi/Pokapi.java
@@ -2,6 +2,7 @@ package pokapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class Pokapi {
