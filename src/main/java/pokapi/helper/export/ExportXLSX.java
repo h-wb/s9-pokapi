a8e4ba68809477d4ca0bf9a302f5535bbf2c6411
Dany Sanchez <daany_57@hotmail.fr>
2020-01-27 01:18:40 +0100

    :bug: Fix bug sonar

--- src/main/java/pokapi/helper/export/ExportXLSX.java
+++ src/main/java/pokapi/helper/export/ExportXLSX.java
@@ -12,40 +12,41 @@ import java.util.List;
 public class ExportXLSX {
 
     public static ByteArrayInputStream export(PokemonsExportVersion pokemonsExportVersion) throws IOException {
-        Workbook workbook = new XSSFWorkbook();
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        Sheet sheet = workbook.createSheet("Pokémons");
+        try (Workbook workbook = new XSSFWorkbook()) {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            Sheet sheet = workbook.createSheet("Pokémons");
 
-        Font headerFont = workbook.createFont();
-        headerFont.setBold(true);
-        headerFont.setFontHeightInPoints((short) 14);
+            Font headerFont = workbook.createFont();
+            headerFont.setBold(true);
+            headerFont.setFontHeightInPoints((short) 14);
 
-        CellStyle headerCellStyle = workbook.createCellStyle();
-        headerCellStyle.setFont(headerFont);
+            CellStyle headerCellStyle = workbook.createCellStyle();
+            headerCellStyle.setFont(headerFont);
 
-        Row headerRow = sheet.createRow(0);
+            Row headerRow = sheet.createRow(0);
 
-        List<String> headerColumns = pokemonsExportVersion.getPokemonsHeaderColumns();
+            List<String> headerColumns = pokemonsExportVersion.getPokemonsHeaderColumns();
 
-        for (int i = 0; i < headerColumns.size(); i++) {
-            Cell cell = headerRow.createCell(i);
-            cell.setCellValue(headerColumns.get(i));
-            cell.setCellStyle(headerCellStyle);
-        }
-
-        int rowNum = 1;
-        for (List<String> pokemonsContentRowsAndColumn : pokemonsExportVersion.getPokemonsContentRowsAndColumns()) {
-            Row row = sheet.createRow(rowNum++);
-            for (int i = 0; i < pokemonsContentRowsAndColumn.size(); i++) {
-                row.createCell(i).setCellValue(pokemonsContentRowsAndColumn.get(i));
+            for (int i = 0; i < headerColumns.size(); i++) {
+                Cell cell = headerRow.createCell(i);
+                cell.setCellValue(headerColumns.get(i));
+                cell.setCellStyle(headerCellStyle);
             }
-        }
 
-        for (int i = 0; i < headerColumns.size(); i++) {
-            sheet.autoSizeColumn(i);
-        }
+            int rowNum = 1;
+            for (List<String> pokemonsContentRowsAndColumn : pokemonsExportVersion.getPokemonsContentRowsAndColumns()) {
+                Row row = sheet.createRow(rowNum++);
+                for (int i = 0; i < pokemonsContentRowsAndColumn.size(); i++) {
+                    row.createCell(i).setCellValue(pokemonsContentRowsAndColumn.get(i));
+                }
+            }
 
-        workbook.write(out);
-        return new ByteArrayInputStream(out.toByteArray());
+            for (int i = 0; i < headerColumns.size(); i++) {
+                sheet.autoSizeColumn(i);
+            }
+
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+        }
     }
 }
